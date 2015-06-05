e84e64a4f26c3a4909d9197698c4fc0e68aaf880
Amit Patil <pamitm@gmail.com>
2015-06-05 19:48:44 +0530

    Added NavActivity

--- app/src/main/java/com/flipkart/age_of_sellers/MainActivity.java
+++ app/src/main/java/com/flipkart/age_of_sellers/MainActivity.java
@@ -12,7 +12,8 @@ import android.view.ViewGroup;
 import android.os.Build;
 
 
-public class MainActivity extends ActionBarActivity {
+public class ls
+        git MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
