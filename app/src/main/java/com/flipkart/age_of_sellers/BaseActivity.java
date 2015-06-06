38008f66d18681df38a108fcc2fc6f573a5b4b63
Manjunath <manjunathc19@gmail.com>
2015-06-06 12:23:40 +0530

    Pushing changes

--- app/src/main/java/com/flipkart/age_of_sellers/BaseActivity.java
+++ app/src/main/java/com/flipkart/age_of_sellers/BaseActivity.java
@@ -65,6 +65,7 @@ public class BaseActivity extends ActionBarActivity
         switch (number) {
             case 1:
                 mTitle = getString(R.string.title_section0);
+                fragment = new HomeFragment();
                 break;
             case 2:
                 mTitle = getString(R.string.title_section1);
@@ -72,9 +73,11 @@ public class BaseActivity extends ActionBarActivity
                 break;
             case 3:
                 mTitle = getString(R.string.title_section2);
+                fragment = new ProfileFragment();
                 break;
             case 4:
                 mTitle = getString(R.string.title_section3);
+                fragment = new CostSheetFragment();
                 break;
         }
         if(fragment != null){
